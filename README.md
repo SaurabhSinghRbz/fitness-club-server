270c1fb03b61c153d87b9eafb0a7c6317edd62ec
Saurabh <singhsaurabh162000@gmail.com>
2022-10-28 00:33:05 +0530

    all crud operation routes are done

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# fitness-club-server
+# _fitness-club-server]()_
+
+## _Response will be like this :_
+
+```json
+{
+  "bodyPart": "waist",
+  "equipment": "body weight",
+  "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/0001.gif",
+  "id": "0001",
+  "name": "3/4 sit-up",
+  "target": "abs"
+}
+```
--- routes/Exercise.routes.js
+++ routes/Exercise.routes.js
@@ -3,6 +3,9 @@ const Exercise = require("../models/Exercise")
 
 
 
+
+
+
 // getAll Exercises...
 ExerciseRouter.get("/exercises", async (req, res) => {
     try {
@@ -12,6 +15,68 @@ ExerciseRouter.get("/exercises", async (req, res) => {
         return res.status(500).send(err)
     }
 })
+
+
+// get one exercise by id...
+ExerciseRouter.get("/exercises/:id", async (req, res) => {
+    try {
+        const exercise = await Exercise.findOne({ id: req.params.id })
+        return res.status(200).send(exercise)
+    } catch (error) {
+        return res.status(500).send(err)
+    }
+})
+
+// create a new exercise...
+ExerciseRouter.post("/exercises", async (req, res) => {
+    try {
+        if (!req.body.password) {
+            return res.status(400).send({ message: "password is required" })
+        } else if (req.body.password === process.env.ADMIN_PASSWORD) {
+            const exercise = await Exercise.create(req.body)
+            return res.status(200).send(exercise)
+        } else {
+            return res.status(401).send("Unauthorized, Please Provide a Valid Password")
+        }
+    } catch (error) {
+        return res.status(500).send(err)
+    }
+
+})
+
+// update an exercise...
+ExerciseRouter.put("/exercises/:id", async (req, res) => {
+    try {
+        if (!req.body.password) {
+            return res.status(400).send({ message: "password is required" })
+        } else if (req.body.password === process.env.ADMIN_PASSWORD) {
+            const exercise = await Exercise.findOneAndUpdate({ id: req.params.id }, req.body, { new: true })
+            return res.status(200).send(exercise)
+        } else {
+            return res.status(401).send("Unauthorized, Please Provide a Valid Password")
+        }
+    } catch (error) {
+        return res.status(500).send(err)
+    }
+})
+
+// delete an exercise...
+ExerciseRouter.delete("/exercises/:id", async (req, res) => {
+    try {
+        if (!req.body.password) {
+            return res.status(400).send({ message: "password is required" })
+        } else if (req.body.password === process.env.ADMIN_PASSWORD) {
+            const exercise = await Exercise.findOneAndDelete({ id: req.params.id })
+            return res.status(200).send(exercise)
+        } else {
+            return res.status(401).send("Unauthorized, Please Provide a Valid Password")
+        }
+    } catch (error) {
+        return res.status(500).send(err)
+    }
+})
+
+
 
 
 module.exports = ExerciseRouter;
